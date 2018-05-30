765d3c11eaf98314de4574e3518984be7cd38151
Rochak Chandra <rochak.chandra96@gmail.com>
2018-05-29 22:16:27 -0400

    Adding Bottom View Of the Tree

--- C++ Files/BST_AdditionalFunctions.cpp
+++ C++ Files/BST_AdditionalFunctions.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <queue>
 #include <stack>
+#include <map>
 
 using namespace std;
 
@@ -9,6 +10,11 @@ typedef struct TreeNode {
 	struct TreeNode * right;
 	struct TreeNode * left;
 }Node;
+
+struct qnode {
+	int distance;
+	Node * node;
+};
 
 Node * newNode(int data) {
 	Node * new_node = new Node();
@@ -33,26 +39,62 @@ bool isCompleteTree(Node * root) {
 	return (left_child == right_child);//XNOR
 }
 
+void BottomViewofTree(Node * root) {
+	if (root == NULL) {
+		cout<<"Tree is empty"<<endl;
+		return;
+	}
+	queue<qnode> q;
+	map <int,int> distance_map;
+	map <int,int> :: iterator i;
+	qnode qitem;
+	qitem.distance = 0;
+	qitem.node = root;
+	q.push(qitem);
+	distance_map.insert(pair <int,int> (qitem.distance,(qitem.node)->key));
+	while (q.empty() != true) {
+		qitem = q.front();
+		Node * tree_node = q.front().node;
+		q.pop();
+		if (tree_node -> left != NULL) {
+			qnode left_node;
+			left_node.distance = qitem.distance - 1;
+			left_node.node = tree_node -> left;
+			q.push(left_node);
+			i = distance_map.find(left_node.distance);
+			if (i != distance_map.end())//If the value is found
+				i -> second = left_node.node -> key;
+			else
+				distance_map.insert(pair <int,int> (left_node.distance,(left_node.node) -> key));
+		}
+		if (tree_node -> right != NULL) {
+			qnode right_node;
+			right_node.distance = qitem.distance + 1;
+			right_node.node = tree_node -> right;
+			q.push(right_node);
+			i = distance_map.find(right_node.distance);
+			if (i != distance_map.end())//If the value is found
+				i -> second = right_node.node -> key;
+			else
+				distance_map.insert(pair <int,int> (right_node.distance,(right_node.node) -> key));
+		}
+	}
+	for (i = distance_map.begin(); i != distance_map.end(); i++)
+		cout<<i -> second<<" ";
+	cout<<endl;
+}
+
 int main() {
 	Node * root = NULL;
-	root = newNode(10);
-    root->left = newNode(20);
-    root->right = newNode(30);
- 
-    root->left->right = newNode(40);
-    root->left->left = newNode(50);
-    root->right->left = newNode(60);
-    root->right->right = newNode(70);
- 
-    root->left->left->left = newNode(80);
-    root->left->left->right = newNode(90);
-    root->left->right->left = newNode(80);
-    root->left->right->right = newNode(90);
-    root->right->left->left = newNode(80);
-    root->right->left->right = newNode(90);
-    //root->right->right->left = newNode(80);
-    //root->right->right->right = newNode(90);
-	bool result = (isCompleteTree(root)) ? true : false;
-	cout<<result<<endl;
+	root = newNode(20);
+	root->left = newNode(8);
+    root->right = newNode(22);
+    root->left->left = newNode(5);
+    root->left->right = newNode(3);
+    root->right->left = newNode(4);
+    root->right->right = newNode(25);
+    root->left->right->left = newNode(10);
+    root->left->right->right = newNode(14);
+	BottomViewofTree(root);
 	return 0;
 }
