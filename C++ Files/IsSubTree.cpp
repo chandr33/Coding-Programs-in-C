26a379697a59f988a5381bb1360a2de407e63400
Rochak Chandra <rochak.chandra96@gmail.com>
2018-05-31 11:05:39 -0400

    Function checks whether a bianry subtree exists

---
+++ C++ Files/IsSubTree.cpp
@@ -0,0 +1,91 @@
+#include <iostream>
+#include <stack>
+#include <string>
+using namespace std;
+
+typedef struct TreeNode {
+	char key;
+	struct TreeNode * left,*right;
+}Node;
+
+Node * newNode(char c) {
+	Node * new_node = new Node();
+	new_node -> key = c;
+	new_node -> left = NULL;
+	new_node -> right = NULL;
+	return new_node;
+}
+//Returns the first occurrence of str2 in str1, otherwise NULL
+char * my_strstr(char * str1, char * str2) {
+	int i =0,j=0,match=0;
+	while (j < strlen(str2)) {
+		if (str1[i] == str2[j]) {
+			match = 1;
+			i++;
+			j++;
+		}
+		else {
+			if (match)
+				return NULL;
+			str1++;
+		}
+	}
+	return str1;
+}
+//Pre-Order Traversal
+void Preorder_Store(Node * tree, char * arr,int * index) {
+	if (tree == NULL)
+		return;
+	arr[*index] = tree -> key;
+	*index = *index + 1;
+	Preorder_Store(tree->left,arr,index);
+	Preorder_Store(tree->right,arr,index);
+}
+
+void Inorder_Store(Node * tree, char * arr, int * index) {
+	if (tree == NULL)
+		return;
+	Inorder_Store(tree->left,arr,index);
+	arr[*index] = tree -> key;
+	*index = *index + 1;
+	Inorder_Store(tree -> right,arr,index);
+}
+
+bool isSubTree(Node * S, Node * T) {
+	char arr_SubInOrder[100];
+	char arr_MainInOrder[100];
+	int i = 0;
+	Inorder_Store(S,arr_SubInOrder,&i);
+	arr_SubInOrder[i] = '\0';
+	i = 0;
+	Inorder_Store(T,arr_MainInOrder,&i);
+	arr_MainInOrder[i] = '\0';
+
+	if (!my_strstr(arr_MainInOrder,arr_SubInOrder))
+		return false;
+
+	char arr_SubPreOrder[100];
+	char arr_MainPreOrder[100];
+	int j = 0;
+	Preorder_Store(S,arr_SubPreOrder,&j);
+	arr_SubPreOrder[j] = '\0';
+	j = 0;
+	Preorder_Store(T,arr_MainPreOrder,&j);
+	arr_MainPreOrder[j] = '\0';
+
+	return strstr(arr_MainPreOrder,arr_SubPreOrder) ? true : false;
+}
+
+int main() {
+	Node *T = newNode('a');
+    T->left = newNode('b');
+    T->left->left = newNode('c');
+    T->right = newNode('d');
+    T->right->right = newNode('e');
+ 
+    Node *S = newNode('a');
+    S->left = newNode('b');
+    S->left->left = newNode('c');
+    S->left->left = newNode('d');
+	cout<<isSubTree(S,T)<<endl; 
+}
