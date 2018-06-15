40868a1aac1e6a297c35d1801287414d2a5dde84
Rochak Chandra <rochak.chandra96@gmail.com>
2018-06-15 15:36:20 -0400

    AVL Tree Implementation Completed

--- C++ Files/AVL.cpp
+++ C++ Files/AVL.cpp
@@ -19,22 +19,22 @@ Tree * newNode(int data) {
 	return new_node;
 }
 
-void InOrder(Tree * root) {
+void PreOrder(Tree * root) {//Prints nodes in Pre-Order Traversal
 	if (!root)
 		return;
-	InOrder(root -> left);
 	cout<<root->key<<" ";
-	InOrder(root -> right);
+	PreOrder(root -> left);
+	PreOrder(root -> right);
 }
 
-int getHeight(Tree * root) {
+int getHeight(Tree * root) {//Returns the height of the node passed
 	if (!root)
 		return 0;
 	return root -> height;
 }
 
-Tree * rotate_right(Tree * x) {
-	Tree * y = x -> left;
+Tree * rotate_right(Tree * x) {//Performs right rotation of parent node with its left child as pivot and updates the
+	Tree * y = x -> left;										//height of the parent node and the left child node
 	Tree * T1 = y -> right;
 	y -> right = x;
 	x -> left = T1;
@@ -43,8 +43,8 @@ Tree * rotate_right(Tree * x) {
 	return y;
 }
 
-Tree * rotate_left(Tree * x) {
-	Tree * y = x -> right;
+Tree * rotate_left(Tree * x) {//Performs left rotation of parent node with its right child as pivot and updates the 
+	Tree * y = x -> right;										//height of the parent node and the right child node
 	Tree * T1 = y -> left;
 	y -> left = x;
 	x -> right = T1;
@@ -53,7 +53,7 @@ Tree * rotate_left(Tree * x) {
 	return y;
 }
 
-int balance(Tree * root) {
+int balance(Tree * root) {//Gets the balance of the passed node
 	if (!root)
 		return 0;
 	return getHeight(root -> left) - getHeight(root -> right);
@@ -107,7 +107,63 @@ Tree * delete_AVL(Tree * root, int data) {
 		root -> key = root -> right -> key;//Replace the root to be deleted's value with its inorder successor
 		root -> right = delete_AVL(root -> right,root -> right -> key);//Recursively call to delete this inorder successor
 	}
+
+	root -> height = max(getHeight(root -> left),getHeight(root -> right)) + 1;//Update all ancestors height
+
+	if ((balance(root) < -1) && (balance(root -> right) <= 0))//Right-Right Case
+		return rotate_left(root);
+
+	if ((balance(root) < -1) && (balance(root -> right) > 0)) {//Right-Left Case
+		root -> right = rotate_right(root -> right);
+		return rotate_left(root);
+	}
+
+	if ((balance(root) > 1) && (balance(root -> left) >= 0))//Left-Left Case
+		return rotate_right(root);
+
+	if ((balance(root) > 1) && (balance(root -> left) < 0)) {//Left-Right Case
+		root -> left = rotate_left(root -> left);
+		return rotate_right(root);
+	}	
 	return root;
+}
+
+//Calculates the height of the binary tree
+int heightOfTree(Tree * root) {
+	int left_height,right_height;
+
+	if (!root)//For an empty tree
+		return 0;
+
+	if ((!root->left) && (!root->right))
+		return 1;
+
+	left_height = (root -> left) ? heightOfTree(root -> left) + 1 : INT_MIN;//Recursive call to increment the height of the left subtree
+	right_height = (root -> right) ? heightOfTree(root -> right) + 1 : INT_MIN;//Recursive call to increment the height of the right subtree
+
+	return max(left_height,right_height);
+}
+
+void search_util(Tree * root, int data, char ** result) {
+	if (!root)//Tree is empty
+		return;
+
+	if (root -> key > data)//Go left
+		search_util(root -> left, data, result);
+
+	if (root -> key < data)//Go right
+		search_util(root -> right, data, result);
+
+	if (root -> key == data)//Data found 
+		strcpy(*result,"Found");//Copy result
+}
+
+char * search(Tree * root, int data) {
+	//static char result[] = "Not found";
+	char * result = new char();//Allocate memory for the result
+	strcpy(result,"Not found");//Assign Default Value
+	search_util(root, data, &result);//Search
+	return result;
 }
 
 int main() {
@@ -118,7 +174,8 @@ int main() {
   	root = insert_AVL(root, 40);
   	root = insert_AVL(root, 50);
   	root = insert_AVL(root, 25);
-  	InOrder(root);
-  	cout<<endl;
+  	cout<<search(root,20)<<endl;
+  	root = delete_AVL(root,20);
+  	cout<<search(root,20)<<endl;
 	return 0;
 }
