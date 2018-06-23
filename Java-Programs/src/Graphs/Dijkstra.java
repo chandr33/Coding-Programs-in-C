23a83f3770d45ab655cfffad60efb4e541b4ae4c
Rochak Chandra <rochak.chandra96@gmail.com>
2018-06-23 19:57:50 -0400

    Implemented Dijkstra using Adjacency Matrix

--- Java-Programs/src/Graphs/Dijkstra.java
+++ Java-Programs/src/Graphs/Dijkstra.java
@@ -1,6 +1,8 @@
 package Graphs;
 import java.util.*;
-
+/* This program prints the Distance and Routes to each vertex of the graph from the given Start Vertex.
+ * The program implements Dijkstra's algorithm using Adjacency Matrix
+ */
 public class Dijkstra {
 	static final int V = 9;
 	static final int INT_MAX = 99999;
@@ -16,17 +18,33 @@ public class Dijkstra {
 		}
 		return min_index;
 	}
-	void printDistance(int [] distance) {
+	void printDistance_util(int [] parent, int src, int dest) {
+		if (src == dest) {
+			System.out.print(dest + " ");
+			return;
+		}
+		printDistance_util(parent, src, parent[dest]);
+		System.out.print(dest + " ");
+		
+	}
+	void printDistance(int [] distance, int [] parent, int startVertex) {
+		System.out.println("Start->End \t Distance \t Route");
 		for (int i = 0; i < V; i++) {
-			System.out.println(i + "         " + distance[i]);
+			if (i != startVertex){
+				System.out.printf("%d->%d \t\t %d \t\t ",startVertex,i,distance[i]);
+				printDistance_util(parent, startVertex, i);
+				System.out.println();
+			}
 		}
 	}
 	
 	void compute(int [][] adj_matrix, int startVertex) {
 		boolean [] visited = new boolean[9];//Array for visited nodes
 		int [] distance = new int[9];//Array for distances from source node
+		int [] parent = new int[9];//Array for storing parent vertices of the shortest route
 		Arrays.fill(distance, INT_MAX);
 		Arrays.fill(visited,false);
+		Arrays.fill(parent, -1);
 		visited[startVertex] = true;
 		distance[startVertex] = 0;
 		int i = startVertex;
@@ -35,8 +53,10 @@ public class Dijkstra {
 			for (int j = 0; j < V; j++) {
 				if (adj_matrix[i][j] != 0) {//If nodes are connected
 					if (!visited[j]){//Check if the node has been visited
-						if ((distance[i] + adj_matrix[i][j]) < distance[j])
+						if ((distance[i] + adj_matrix[i][j]) < distance[j]) {
 							distance[j] = distance[i] + adj_matrix[i][j];//Update the distance
+							parent[j] = i;
+						}
 					}
 				}
 			}
@@ -44,7 +64,7 @@ public class Dijkstra {
 			visited[i] = true;
 			count++;
 		}
-		printDistance(distance);
+		printDistance(distance,parent,startVertex);
 	}
 	public static void main(String[] args) {
 		int graph[][] = new int[][]{{0, 4, 0, 0, 0, 0, 0, 8, 0},
@@ -58,6 +78,6 @@ public class Dijkstra {
 	                                {0, 0, 2, 0, 0, 0, 6, 7, 0}};
 		
 		Dijkstra d = new Dijkstra();
-		d.compute(graph, 0);
+		d.compute(graph, 4);
 	}
 }
